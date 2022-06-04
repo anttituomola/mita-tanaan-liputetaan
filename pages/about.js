9474e6fdb7e58570b97ed1e4a2ac184139942df3
Antti <anttituomola8@gmail.com>
2022-06-04 13:22:01 +0300

    link to Github

--- pages/about.js
+++ pages/about.js
@@ -6,6 +6,7 @@ const about = () => {
         <h3><a href="https://anttituomola.fi" target="_blank" rel="noreferrer">Antti Tuomola</a> on keski-ikäistyvä ohjelmistokehittäjä, 
         joka kyllästyi miettimään, mitä tänään liputetaan.</h3>
         <p>Tarvitsetko käyttöösi Liputuspäivät-API:n? Teen sen mielelläni! Laita viestiä: antti@anttituomola.fi</p>
+        <p>Huomasitko virheen? Puske korjaus sisään <a href="https://github.com/anttituomola/mita-tanaan-liputetaan" target="_blank" rel="noreferrer">Githubissa</a>! </p>
     </main>
   )
 }
