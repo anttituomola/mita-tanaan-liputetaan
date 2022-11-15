9141b16aa5fc0f8a60b816955caec132c46e964e
Antti <antti.tuomola@gofore.com>
2022-11-15 13:40:23 +0200

    fixed URLs

--- pages/rajapinta-api.js
+++ pages/rajapinta-api.js
@@ -5,10 +5,10 @@ const api = () => {
             <main className="container2">
                 <h1>Rajapinta eli API</h1>
                 <p>mitätänäänliputetaan.fi tarjoaa käyttöösi avoimen rajapinnan Suomen liputuspäivädataan.</p>
-                <p>Voit hakea liputuspäivätiedot JSON-muodossa osoitteesta <a href="https://mitatanaanliputetaan.fi/api/liputuspäivät">https://mitatanaanliputetaan.fi/api/liputuspaivat</a></p>
-                <p>Kuluvan viikon liputuspäivät <a href="https://mitatanaanliputetaan.fi/api/liputuspäivät/thisWeek">https://mitatanaanliputetaan.fi/api/liputuspaivat/thisWeek</a></p>
-                <p>Kuluvan kuun liputuspäivät <a href="https://mitatanaanliputetaan.fi/api/liputuspäivät/thisMonth">https://mitatanaanliputetaan.fi/api/liputuspaivat/thisMonth</a></p>
-                <p>Kuluvan päivän liputuspäivät <a href="https://mitatanaanliputetaan.fi/api/today">https://mitatanaanliputetaan.fi/api/today</a></p>
+                <p>Voit hakea liputuspäivätiedot JSON-muodossa osoitteesta <a href="https://mitatanaanliputetaan.vercel.app/api/liputuspäivät">https://mitatanaanliputetaan.vercel.app/api/liputuspaivat</a></p>
+                <p>Kuluvan viikon liputuspäivät <a href="https://mitatanaanliputetaan.vercel.app/api/liputuspäivät/thisWeek">https://mitatanaanliputetaan.vercel.app/api/liputuspaivat/thisWeek</a></p>
+                <p>Kuluvan kuun liputuspäivät <a href="https://mitatanaanliputetaan.vercel.app/api/liputuspäivät/thisMonth">https://mitatanaanliputetaan.vercel.app/api/liputuspaivat/thisMonth</a></p>
+                <p>Kuluvan päivän liputuspäivät <a href="https://mitatanaanliputetaan.vercel.app/api/today">https://mitatanaanliputetaan.vercel.app/api/today</a></p>
             </main>
 
         </div>
